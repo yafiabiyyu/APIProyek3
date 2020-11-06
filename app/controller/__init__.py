690d150c27d062d05fb5fbbf406d17188e1b88f7
Abiyyu Yafi <luckyveri3@gmail.com>
2020-11-06 13:49:20 +0700

    add subkriteria namespace

--- app/controller/__init__.py
+++ app/controller/__init__.py
@@ -4,8 +4,10 @@ from flask_restplus import Api
 # import namespace
 from .auth_controller import api as auth_ns
 from .kriteria_controller import api as kriteria_ns
+from .subkriteria_controller import api as subkriteria_ns
 
 controller = Blueprint("api", __name__)
 api = Api(controller, version="1.0", title="RESTAPI untuk proyek 3")
 api.add_namespace(auth_ns, path="/proyek/user")
 api.add_namespace(kriteria_ns, path="/proyek/kriteria")
+api.add_namespace(subkriteria_ns, path="/proyek/subkriteria")
