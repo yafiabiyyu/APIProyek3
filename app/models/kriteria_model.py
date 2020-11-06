8b99254e8cbbebad2d36fe8d7b1636da5582ea6c
Abiyyu Yafi <luckyveri3@gmail.com>
2020-11-06 13:48:13 +0700

    set primary key

--- app/models/kriteria_model.py
+++ app/models/kriteria_model.py
@@ -2,7 +2,16 @@ from app import db
 
 
 class KriteriaDoc(db.Document):
-    _id = db.StringField(required=True, max_length=15)
+    _id = db.StringField(required=True, primary_key=True, max_length=15)
     nama_kriteria = db.StringField(required=True, max_length=50)
     atribut = db.StringField(required=True, max_length=10)
     bobot = db.FloatField(required=True)
+
+
+class SubkriteriaDoc(db.Document):
+    _id = _id = db.StringField(
+        required=True, primary_key=True, max_length=15
+    )
+    jenis_kriteria = db.ReferenceField("KriteriaDoc")
+    nama_subkriteria = db.StringField(required=True, max_length=50)
+    nilai = db.FloatField(required=True)
