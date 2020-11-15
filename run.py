1c31a4c4f56b760911d089b79b68df7a8217461b
Abiyyu Yafi <luckyveri3@gmail.com>
2020-11-15 13:03:56 +0700

    update run.py

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ from app import create_app
 
 config_name = os.getenv("FLASK_ENV")
 app = create_app(config_name)
-
+port = os.getenv('PORT')
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=port, debug=True)
